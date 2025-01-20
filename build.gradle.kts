12b409697bbde4eb82bab8ca7e0748bbe2a0ffeb
2894638479 <2894638479@qq.com>
2025-01-20 19:04:53 +0800

    去除jvm源集

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,9 @@
 plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version "2.1.20-Beta1"
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.composeMultiplatform) apply false
-    alias(libs.plugins.composeCompiler) apply false
 }
 
 repositories {
-    google()
     mavenCentral()
 }
 
@@ -19,22 +16,7 @@ kotlin {
             }
         }
         nativeMain
-        jvmMain {
-            dependencies {
-//                implementation(compose.runtime)
-//                implementation(compose.foundation)
-//                implementation(compose.material)
-//                implementation(compose.ui)
-//                implementation(compose.components.resources)
-//                implementation(compose.components.uiToolingPreview)
-//                implementation(libs.androidx.lifecycle.viewmodel)
-//                implementation(libs.androidx.lifecycle.runtime.compose)
-//                implementation(compose.desktop.currentOs)
-//                implementation(libs.kotlinx.coroutines.swing)
-            }
-        }
     }
-    jvm("jvm")
     mingwX64("nativeX64"){
         compilations.getByName("main"){
             cinterops {
@@ -47,7 +29,7 @@ kotlin {
         }
         binaries{
             executable(listOf(RELEASE)){
-//                linkerOpts += "-mwindows"
+                linkerOpts += "-mwindows"
             }
         }
         tasks.named("linkReleaseExecutableNativeX64"){
