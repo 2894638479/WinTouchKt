554412c1fbbb88e8f66b70e79926cc4ca37d3048
2894638479 <2894638479@qq.com>
2025-10-04 16:05:15 +0800

    4.0.1

--- src/nativeX64Main/kotlin/Main.kt
+++ src/nativeX64Main/kotlin/Main.kt
@@ -14,7 +14,7 @@ import window.registerLayered
 
 
 const val VERSION = "4.0"
-const val VERSION_SUFFIX = ".0"
+const val VERSION_SUFFIX = ".1"
 
 @OptIn(ExperimentalForeignApi::class)
 fun main() = catchInKotlin {
